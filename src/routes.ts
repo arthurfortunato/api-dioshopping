8db98793f0b7bab49b229bcca2f42d7ad48bed50
arthurfortunato <luizarthur.fortunato@gmail.com>
2022-02-04 14:02:21 -0300

    Criando o método de obter as mensagens

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,17 @@
 import { Router, Request, Response } from "express";
 import { CreateMessageController } from "./controllers/CreateMessageController";
+import { ListMessageController } from "./controllers/ListMessageController";
 
 const router = Router();
 
 const createMessageController = new CreateMessageController();
+const listMessageController = new ListMessageController();
 
 router.get('/', (request: Request, response: Response) => {
     return response.json({message: 'Bem vindo a API Dio Shopping'})
 })
 
 router.post('/message', createMessageController.handle)
+router.get('/message', listMessageController.handle)
 
 export { router }
