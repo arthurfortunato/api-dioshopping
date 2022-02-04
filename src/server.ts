cb6b6bb5ce382a8844d0d7ed66e5d05f12b86267
arthurfortunato <luizarthur.fortunato@gmail.com>
2022-02-04 14:04:55 -0300

    Instalando o cors

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
-import express from 'express';
 import "reflect-metadata";
+import express from 'express';
 import { createConnection } from 'typeorm';
 import { router } from "./routes";
+import cors from "cors";
 
 createConnection().then(() => {
   const app = express();
   const PORT = 4000;
 
   app.use(express.json());
-  app.use(router)
+  app.use(router);
+  app.use(cors());
 
   app.listen(PORT, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
